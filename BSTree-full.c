dac5106bb48e218db4feae32e42415acce96d600
HokHun <hokhun078@gmail.com>
2022-12-25 15:08:48 +0700

    update

--- BSTree-full.c
+++ BSTree-full.c
@@ -92,8 +92,60 @@ bool check_element( node * root, int key) {
         return check_element(root -> right, key);
     }
 }
+void search_all(node* root, int key, int pos) {
+// Nếu nút hiện tại là NULL, không có gì để tìm
+    if (root == NULL) {
+        return;
+    }
+    // Nếu phần tử của nút hiện tại trùng với giá trị cần tìm, in ra giá trị và vị trí đó
+    if (root -> data == key) {
+        printf("%d (vi tri %d) ", root -> data, pos);
+    }
+    // Tăng biến đếm lên 1 và tiếp tục tìm trong cây con trái và cây con phải
+    search_all(root -> left, key, pos + 1);
+    search_all(root -> right, key, pos + 1);
+}
+// them vao dau
+node* insert_head(node* root, int data) {
+// Nếu cây rỗng, tạo một nút mới với giá trị data và trả về nút đó
+    if (root == NULL) {
+        return newNode(data);
+    }
+    // Tạo một nút mới với giá trị data
+    node* new_node = newNode(data);
+    // Đặt nút mới là nút gốc của cây hiện tại
+    new_node -> right = root;
+    // Trả về nút mới làm nút gốc của cây
+    return new_node;
+}
+// the vao vi tri bat ki
+void insert_at_position(node** root, int data, int pos) {
+// Nếu cây hiện tại là NULL, tạo nút mới với giá trị là data và gán cho cây
+    if (*root == NULL) {
+        *root = newNode(data);
+        return;
+    }
+    // Nếu vị trí hiện tại là 1, tức là cần chèn phần tử vào nút gốc,
+    // thì tạo nút mới với giá trị là data và thay thế nút gốc bằng nút mới
+    if (pos == 1) {
+        node *temp = newNode(data);
+        temp -> left = *root;
+        *root = temp;
+        return;
+    }
+    // Nếu vị trí hiện tại là lẻ, tức là cần chèn phần tử vào cây con bên trái
+    // điều hướng đến cây con bên trái và giảm vị trí hiện tại xuống 1
+    if (pos % 2 == 1) {
+        insert_at_position(&((*root) -> left), data, pos / 2);
+    }
+    // Nếu vị trí hiện tại là chẵn, tức là cần chèn phần tử vào cây con bên phải
+    // điều hướng đến cây con bên phải và giảm vị trí hiện tại xuống 1
+    else {
+        insert_at_position(&((*root) -> right), data, pos / 2);
+    }
+}
 int main() {
-    int n, i,a,b;
+    int n, i,a,b,pos,data;//pos va data de tim kiem phan tu vi tri bat ki
     printf("nhap so luong phan tu: ");
     scanf("%d", &n);
     node *root = NULL;
@@ -115,15 +167,34 @@ int main() {
     else {
         printf("Khong tim thay\n");
     }
-    printf("Nhap phan tu can them: ");
+    printf("nhap phan tu can chen tai vi tri bat ki : ");
+    scanf("%d",&data);
+    printf("nhap vi tri phan tu can chen: ");
+    scanf("%d",&pos);
+    insert_at_position(&root, data, pos);
+    print_tree(root);
+    printf("Nhap phan tu can them vao cuoi: ");
     scanf("%d", &a);
     root = insert(root,a);
     printf("Cay sau khi them phan tu la: ");
     print_tree(root);
-    printf("Nhap phan tu can xoa: ");
+    printf("\nNhap phan tu can xoa : ");
     scanf("%d", &b);
     root = delete_node(root, b);
     printf("Cay sau khi xoa phan tu la: ");
+    print_tree(root);
+    int key;
+    printf("\nNhap phan tu can tim : ");
+    scanf("%d",&key);
+    // Gọi hàm tìm kiếm tất cả các phần tử giống nhau
+    printf("Cac phan tu trong cay giong nhau la: ");
+    search_all(root, key, 0);
+    printf("\n");
+    printf("nhap phan tu can them vao dau : ");
+    scanf("%d",&key);
+    root = insert_head(root, key);
+    // In cây
+    printf("Cay sau khi them vao dau: ");
     print_tree(root);
     return 0;
 }
